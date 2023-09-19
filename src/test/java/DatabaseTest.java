2c2507700fcfd90c6bf8df36fee6fc6f781e93ba
NikoP93 <nipe0001@stud.kea.dk>
2023-09-19 15:47:01 +0200

    Initial commit

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+class DatabaseTest {
+
+    @org.junit.jupiter.api.Test
+    void getSuperherolist() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addSuperhero() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findSuperhero() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findSuperheroes() {
+    }
+}
