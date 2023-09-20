ca3d34ca321be7450b68401979f37d3974fe9521
NikoP93 <nipe0001@stud.kea.dk>
2023-09-20 12:27:26 +0200

    User story 10 Delete superhero

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Database {
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Database {
+
+
+    //Array
+    private ArrayList<Superhero> superherolist;
+
+
+    //array
+    public Database() {
+        this.superherolist = new ArrayList<>();
+    }
+
+    //get metode
+    public ArrayList<Superhero> getSuperherolist() {
+        return superherolist;
+    }
+
+    //metode til at adde til arraylist
+    public void addSuperhero(Superhero superhero) {
+        superherolist.add(superhero);
+    }
+
+    public Superhero findSuperhero(String superheroName){
+        for (Superhero hero : superherolist) {
+            if (hero.getName().contains(superheroName))
+                return hero;
+        }
+
+        return null;
+    }
+
+    public ArrayList<Superhero> findSuperheroes(String superheroName){
+        ArrayList<Superhero> superheroes = new ArrayList<>();
+        for (Superhero superhero : superherolist) {
+            if (superhero.getName().contains(superheroName)){
+                superheroes.add(superhero);
+            }
+        }
+        return superheroes;
+    }
+
+    public ArrayList<Superhero> deleteSuperhero(String superheroName){
+        Iterator<Superhero> itr = superherolist.iterator();
+        while (itr.hasNext()){
+            Superhero superhero = itr.next();
+            if (superhero.getName().equals(superheroName)){
+                itr.remove();
+            }
+        }
+        return superherolist;
+
+    }
+
+
+
+
 }
--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,19 +1,75 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DatabaseTest {
+    Database db;
 
-    @org.junit.jupiter.api.Test
-    void getSuperherolist() {
+    //udføres en gang for hver test
+
+    @BeforeEach
+    void setUP(){
+        //text fixture
+        db = new Database();
+        db.getSuperherolist().addAll(List.of(
+                new Superhero("Superman", "Kent Clark", "Flying", 1938, "No", "9000"),
+                new Superhero("Batman", "Bruce Wayne", "Strong", 1920, "Yes", "200"),
+                new Superhero("Ironman", "Tony Stark", "Robot", 1995, "Yes", "3000")));
     }
 
-    @org.junit.jupiter.api.Test
-    void addSuperhero() {
-    }
-
-    @org.junit.jupiter.api.Test
+    @Test
     void findSuperhero() {
+        //Arrange test
+        String expectedName = "Superman";
+        //act on method
+        //metode der tester "find superhero"
+        Superhero result = db.findSuperhero("Superman");
+        //tester at result ikke er null
+        assertNotNull(result);
+        String actualName = result.getName();
+        //assert result
+        assertEquals(expectedName,actualName);
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void findSuperheroes() {
+        // AssertTrue(false)
+        int expectedSize = 3;
+        ArrayList<Superhero> result = db.findSuperheroes("man");
+        int actualSize=result.size();
+        assertEquals(expectedSize,actualSize);
     }
+
+    @Test
+    void addSuperhero() {
+        //tilføj superhero
+        int startSize = db.getSuperherolist().size();
+        db.addSuperhero(new Superhero("Aquaman", "Jens", "Fish", 1999, "No", "9000"));
+        int expectedDbSize = startSize +1;
+        int actualSize = db.getSuperherolist().size();
+        assertEquals(expectedDbSize,actualSize);
+    }
+
+    @Test
+    void deleteSuperhero() {
+        int startSize = db.getSuperherolist().size();
+        db.deleteSuperhero("Batman");
+        int expectedDbSize = startSize -1;
+        int actualSize = db.getSuperherolist().size();
+        assertEquals(expectedDbSize,actualSize);
+        System.out.println(db.getSuperherolist());
+    }
+
+
+
+
+
+
+
+
 }
